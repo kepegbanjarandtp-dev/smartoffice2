d3fba9ddbe553e5a0da83b84c546321510af9b9c
kepegbanjarandtp-dev <kepeg.banjarandtp@gmail.com>
2026-08-12 16:19:52 +0700

    Add files via upload

--- src/core/config.js
+++ src/core/config.js
@@ -2,17 +2,10 @@
    SMARTOFFICE CONFIG
 ====================================================== */
 export const CONFIG = {
+    APP_NAME: "SmartOffice V2",
+    APP_VERSION: "1.0.0",
 
-    APP_NAME :
-        "SmartOffice V2",
+    API_URL: "/api/gas",
 
-    APP_VERSION :
-        "1.0.0",
-
-    API_URL :
-        "https://script.google.com/macros/s/AKfycbxqLDIwWjg9Ch5ADVEztwhcUllQoaTs-uUcNzYaWujeMkYYabsTvz7RhkSl78UNXzkz/exec",
-
-    DEBUG :
-        true
-
+    DEBUG: true
 };
