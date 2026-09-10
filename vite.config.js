7747094fdd5738c3b5ad2db3127433756e94cb17
kepegbanjarandtp-dev <kepeg.banjarandtp@gmail.com>
2026-09-10 14:35:38 +0700

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,19 @@ import { VitePWA } from "vite-plugin-pwa";
 export default defineConfig({
 
     plugins: [
+
         VitePWA({
+
+            strategies: "injectManifest",
+
+            srcDir: "src",
+
+            filename: "sw.js",
+
             registerType: "autoUpdate",
+
             injectRegister: "auto",
+
             includeAssets: [
                 "smartoffice-icon-192-maskable.png",
                 "smartoffice-icon-512-maskable.png",
@@ -15,28 +25,49 @@ export default defineConfig({
             ],
 
             manifest: {
-                name: "SmartOffice V2",
-                short_name: "SmartOffice V2",
+
+                name:
+                    "SmartOffice V2",
+
+                short_name:
+                    "SmartOffice V2",
+
                 description:
                     "Smart Office Puskesmas",
-                lang: "id-ID",
-                start_url: "/",
-                scope: "/",
-                display: "standalone",
+
+                lang:
+                    "id-ID",
+
+                start_url:
+                    "/",
+
+                scope:
+                    "/",
+
+                display:
+                    "standalone",
+
                 orientation:
                     "portrait-primary",
+
                 background_color:
                     "#ffffff",
+
                 theme_color:
                     "#ffffff",
+
                 icons: [
+
                     {
                         src:
                             "/smartoffice-icon-192-maskable.png",
+
                         sizes:
                             "192x192",
+
                         type:
                             "image/png",
+
                         purpose:
                             "maskable"
                     },
@@ -44,24 +75,22 @@ export default defineConfig({
                     {
                         src:
                             "/smartoffice-icon-512-maskable.png",
+
                         sizes:
                             "512x512",
+
                         type:
                             "image/png",
+
                         purpose:
                             "maskable"
                     }
-                ]
-            },
 
-            workbox: {
-                cleanupOutdatedCaches:
-                    true,
-                clientsClaim:
-                    true,
-                skipWaiting:
-                    true
+                ]
             }
+
         })
+
     ]
+
 });
