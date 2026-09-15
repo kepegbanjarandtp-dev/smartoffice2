bd80eb60fe035b5c39cb6763c17b8ef5151182b1
kepegbanjarandtp-dev <kepeg.banjarandtp@gmail.com>
2026-09-15 17:06:37 +0700

    Update config.js

--- src/core/config.js
+++ src/core/config.js
@@ -10,7 +10,7 @@ export const CONFIG = {
         "3.0.0",
 
     API_URL :
-        "https://script.google.com/macros/s/AKfycby2Bpu2m5alqOsvxiTrKHWGu3g2g3xX6y5s09fqFvq6AjRd-AzHb_HDiyUjl6gjKF6GsA/exec",
+        "https://script.google.com/macros/s/AKfycbxqLDIwWjg9Ch5ADVEztwhcUllQoaTs-uUcNzYaWujeMkYYabsTvz7RhkSl78UNXzkz/exec",
 
     DEBUG :
         true
